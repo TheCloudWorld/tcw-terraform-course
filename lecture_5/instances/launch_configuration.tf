e6cdd55d42cc104e89f4ab98428fc0fef952456a
wilshan <74950445+wilshan@users.noreply.github.com>
2021-09-04 08:41:25 +0000

    Update launch_configuration.tf

--- lecture_5/instances/launch_configuration.tf
+++ lecture_5/instances/launch_configuration.tf
@@ -1,26 +1,17 @@
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+data "aws_ami" "amazon_linux_2" {
+ most_recent = true
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
 }
 
-resource "aws_launch_configuration" "as_conf" {
-  name_prefix   = "terraform-lc-example-"
-  image_id      = data.aws_ami.ubuntu.id
+resource "aws_launch_configuration" "lc" {
+  name_prefix   = "tcw_lc"
+  image_id      = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
