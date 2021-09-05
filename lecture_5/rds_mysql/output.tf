f00af096f6368ca9037d5fdad81423b03f3133ba
wilshan <74950445+wilshan@users.noreply.github.com>
2021-09-05 12:15:17 +0000

    Update output.tf

--- lecture_5/rds_mysql/output.tf
+++ lecture_5/rds_mysql/output.tf
@@ -1,5 +1,5 @@
-output "rds_endpoint" {
-  value = aws_db_instance.db_instance.endpoint
+output "subnet_data" {
+  value = data.aws_subnet_ids.available_db_subnet.ids
 }
 output "rds_address" {
   value = aws_db_instance.db_instance.address
